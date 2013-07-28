0a59a42cb3bf0f89ef36b369677efc9f3491a9a3
Wayne W <ww.exe@163.com>
2013-07-28 09:49:23 +0800

    add project

---
+++ P2PPlayer4iOS/P2PPlayer4iOS.h
@@ -0,0 +1,13 @@
+//
+//  P2PPlayer4iOS.h
+//  P2PPlayer4iOS
+//
+//  Created by Wayne W on 13-7-28.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface P2PPlayer4iOS : UIView
+
+@end
---
+++ P2PPlayer4iOS/P2PPlayer4iOS.m
@@ -0,0 +1,13 @@
+//
+//  P2PPlayer4iOS.m
+//  P2PPlayer4iOS
+//
+//  Created by Wayne W on 13-7-28.
+//
+//
+
+#import "P2PPlayer4iOS.h"
+
+@implementation P2PPlayer4iOS
+
+@end
